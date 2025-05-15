d1b1fd6f0b49f4b972358e02acd5c2933c5e54ff
Alba Sánchez <albasanchezolmedo00@gmail.com>
2025-05-15 22:35:01 +0200

    Actualizar rutas de hojas de estilo en header.php para referencia absoluta

--- proyectoAdidas/header.php
+++ proyectoAdidas/header.php
@@ -9,7 +9,7 @@ session_start();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Adidas</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/proyectoAdidas/css/style.css">
     <link rel="icon" type="image/png" href="media/favicon.png">
 </head>
 <body>
--- proyectoCepa/vista/header.php
+++ proyectoCepa/vista/header.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Formulario Alta</title>
-    <link rel="stylesheet" href="../vista/css/style.css">
+    <link rel="stylesheet" href="/proyectoCepa/vista/css/style.css">
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
 </head>
 <body>
